65d2580ae02dbdb3eaaf1acfb7848ad5c7c49347
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-01-14 08:20:39 +0100

    Add files via upload

--- 03.jl
+++ 03.jl
@@ -5,15 +5,17 @@ open("03.txt") do f
         ss = split(split(l,"@ ")[2], ": ")
         x, y = parse.(Int, split(ss[1], ","))
         b, h = parse.(Int, split(ss[2], "x"))
-        fabric[x+1:x+b, y+1:y+h] .+= 1
-        push!(claims, (x,y,b,h))
+        xr, yr = x+1:x+b, y+1:y+h
+        fabric[xr, yr] .+= 1
+        push!(claims, (xr, yr))
     end
     println("Part 1: ", sum(fabric .> 1))
 
-    for (i, (x,y,b,h)) ∈ enumerate(claims)
-        if all(fabric[x+1:x+b, y+1:y+h].== 1)
-            println("Part 2: ", i)
-            break
-        end
-    end
+    println("Part 2: ", findfirst(c->all(fabric[c...] .== 1), claims))
+#    for (i, c) ∈ enumerate(claims)
+#        if all(fabric[c...] .== 1)
+#            println("Part 2: ", i)
+#            break
+#        end
+#    end
 end

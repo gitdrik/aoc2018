276f36673b3ebc1a860768574ac3a1f31c042dcd
gitdrik <58667795+gitdrik@users.noreply.github.com>
2021-01-19 20:47:31 +0100

    Add files via upload

--- 12.jl
+++ 12.jl
@@ -1,21 +1,18 @@
 open("12.txt") do f
     ls = readlines(f)
-    ostate = [c=='#' for c ∈ ls[1][16:end]]
     rules = Dict{Array{Bool},Bool}([c=='#' for c ∈ s[1:5]]=>s[10]=='#' for s in ls[3:34])
-
-    state = [falses(4);copy(ostate);falses(20)]
+    state = [falses(4);[c=='#' for c ∈ ls[1][16:end]];falses(30)]
     for t ∈ 1:20
         state = [[false,false];[rules[state[i:i+4]] for i ∈ 1:length(state)-4];[false,false]]
     end
     println("Part 1: ", sum((-4:length(state)-5)[state]))
 
-    reps = 50000000000
-    state = [falses(4);copy(ostate);falses(50)]
-    for t ∈ 1:113
+    for t ∈ 21:113
         state = [[false,false];[rules[state[i:i+4]] for i ∈ 1:length(state)-4];[false,false,false]]
     end
-    # from t=113 all rows are the same, the plants just move one step right/generation
+    reps = 50000000000
     rsteps = reps-113
-    sum2 = sum((rsteps-4:rsteps+length(state)-5)[state])
-    println("Part 2: ", sum2)
+    # As it happens, starting at gen 113 the plant congfiguration stays the same,
+    # just moving one step to the right per generation.
+    println("Part 2: ", sum((rsteps-4:rsteps+length(state)-5)[state]))
 end
